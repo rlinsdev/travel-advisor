ebda241b732b0bffaf941ddd072ea9d6bb2dc806
Rodrigo Lins <rlinsdev@gmail.com>
2021-08-08 20:08:54 -0300

    Styling

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutLinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map  = () => {
   return(
