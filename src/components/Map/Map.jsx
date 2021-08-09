330d17012045dbba692a8efb2b6ce7b545ce40f3
Rodrigo Lins <rlinsdev@gmail.com>
2021-08-08 21:12:21 -0300

    Component - List

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutLinedIcon from '@material-ui/icons/LocationOutlined';
+import LocationOnOutLinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
 
 const Map  = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordenates = { lat: -22.0123, lng: -47.8908 };
+
   return(
-    <h1>Header</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact 
+        bootstrapURLKeys={{key: 'AIzaSyCTV8UdEdXc4hta6NsVO9QXo29L43w_4iI'}}
+        defaultCenter={coordenates}
+        center={coordenates}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        // options={}
+        // onChange={}
+        // onChildClick={}
+      >
+
+      </GoogleMapReact>
+    </div>
   );
 }
 export default Map;
