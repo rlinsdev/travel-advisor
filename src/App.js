d43ad229df12a451ac9ab95dcda562019aff1bf5
Rodrigo Lins <rlinsdev@gmail.com>
2021-08-11 08:31:57 -0300

    Fixing Filters

--- src/App.js
+++ src/App.js
@@ -30,15 +30,19 @@ const App = () => {
   },[rating])
 
   useEffect (() => {
-    setIsLoading(true);
-    getPlacesData(type, bounds.sw, bounds.ne)
-      .then((data) => {
-        //console.log(data); // Dado recuperado da API
-        setPlaces(data);
-        setFilteredPlaces([]);
-        setIsLoading(false);
-      });
-  }, [type, coordinates, bounds]);
+
+    if(bounds.sw && bounds.ne) {
+      setIsLoading(true);
+      getPlacesData(type, bounds.sw, bounds.ne)
+        .then((data) => {
+          //console.log(data); // Dado recuperado da API
+          setPlaces(data?.filter((place)=> place.name && place.num_reviews > 0));
+          setFilteredPlaces([]);
+          setIsLoading(false);
+        });
+      }
+
+  }, [type, bounds]);
 
   return (
     <>
